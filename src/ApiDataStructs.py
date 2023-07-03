09958231f73256e1a974f540a0975d3bbc61a2ff
hyi <hongyi@renci.org>
2023-07-03 16:17:06 -0400

    trying to get API working

--- src/ApiDataStructs.py
+++ src/ApiDataStructs.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
 
-class Query(BaseModel):
-    text: str
-    model_name: str
-    # count is not required and will default to 1 if nothing is passed
-    count: int = 1
+class ModelResponse(BaseModel):
+    name: str
+
+
+class DataInfoResponse(BaseModel):
+    name: str
+    total_slices: int
--- src/server.py
+++ src/server.py
@@ -1,9 +1,13 @@
 import os
-
+import json
+import numpy as np
 from fastapi import FastAPI
+from fastapi import HTTPException
 import uvicorn
 import logging
-import yaml
+from src.ConfigSingleton import init_configuration, SAMConfig
+from src.ApiDataStructs import ModelResponse, DataInfoResponse
+
 
 app = FastAPI()
 logger = logging.getLogger('gunicorn.error')
@@ -18,16 +22,34 @@ def load_info():
                                           'config.yaml'
                                       )
                                       )
-    with open(config_file_path) as config_stream:
-        config = yaml.load(config_stream, Loader=yaml.SafeLoader)
-        logger.info(config)
+    init_configuration(config_file_path)
 
 
 @app.get("/models/")
-async def get_model_names():
-    """
-    """
-    return list(['ViT_H SAM model'])
+async def get_model_names() -> ModelResponse:
+    return {'name': SAMConfig.models['name']}
+
+
+@app.get("/data_info/")
+async def get_data_info() -> list[DataInfoResponse]:
+    ret = []
+    for key, val in SAMConfig.data.items():
+        ret.append({
+            'name': key,
+            'total_slices': val['total_slices']
+        })
+    return ret
+
+
+@app.get("/data_slice_embedding/{data_name}/{slice_no}")
+async def get_data_slice_embedding(data_name: str, slice_no: int):
+    if data_name not in SAMConfig.data:
+        raise HTTPException(status_code=404, detail="input data name not found")
+    embed_file = SAMConfig.data[data_name]['embedding'].format(slice_no)
+    if not os.path.isfile(embed_file):
+        raise HTTPException(status_code=404, detail="input slice_no is invalid")
+    embed_data = np.load(embed_file)
+    return json.dumps(embed_data.tolist())
 
 
 if __name__ == '__main__':
